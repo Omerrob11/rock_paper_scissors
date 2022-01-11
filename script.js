a4d2c860a2a2fb3d672db72e00868c0c34030e9d
Omer Miloh <omerrob11@gmail.com>
2022-01-11 15:40:00 +0200

    Printing round winner

--- script.js
+++ script.js
@@ -19,11 +19,49 @@ function getPlayerSelection() {
         "Error, please check a value that is one of the possible 3 choices"
       );
     }
-    debugger;
   } while (
     playerChoice !== "paper" &&
     playerChoice !== "rock" &&
     playerChoice !== "scissors"
   );
   return playerChoice;
+}
+
+function getComputerChoice() {
+  let min = 1;
+  let max = 3;
+  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "rock";
+  } else {
+    return "scissors";
+  }
+}
+
+function printPlayersPicks(playerSelection, computerSelection) {
+  let playersPicksMessage = `Player Select ${playerSelection}, Computer Select ${computerSelection}`;
+
+  return playersPicksMessage;
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (
+    (computerChoice === "rock" && playerChoice === "paper") ||
+    (computerChoice === "scissors" && playerChoice === "rock") ||
+    (computerChoice === "paper" && playerChoice === "scissors")
+  ) {
+    return "player";
+  } else if (playerChoice === computerChoice) {
+    return "tie";
+  } else {
+    return "computer";
+  }
+}
+
+function printRoundWinner(winner) {
+  let roundWinnerMessage = `And the winner of this round is ${winner}`;
+  return roundWinnerMessage;
 }
