685e6fe020eab9ce6b46cbad7171365e2ec6e5ff
Omer Miloh <omerrob11@gmail.com>
2022-01-11 15:35:10 +0200

    Change player Selection function to while loop

--- script.js
+++ script.js
@@ -5,22 +5,25 @@ let computerPoints = 0;
 let userPoints = 5;
 
 function getPlayerSelection() {
-  let playerChoice = prompt("Please choose between Rock, Paper, Scissors");
+  let playerChoice = null;
+  do {
+    playerChoice = prompt("Please choose between Rock, Paper, Scissors");
+    playerChoice = playerChoice === null ? null : playerChoice.toLowerCase();
 
-  playerChoice = playerChoice === null ? null : playerChoice.toLowerCase();
-  if (
+    if (
+      playerChoice !== "paper" &&
+      playerChoice !== "rock" &&
+      playerChoice !== "scissors"
+    ) {
+      console.log(
+        "Error, please check a value that is one of the possible 3 choices"
+      );
+    }
+    debugger;
+  } while (
     playerChoice !== "paper" &&
     playerChoice !== "rock" &&
     playerChoice !== "scissors"
-  ) {
-    // Here, prompt call the function that call it again
-
-    console.log(
-      "Error, please check a value that is one of the possible 3 choices"
-    );
-    // You change dive here or call function to change div here
-    return "error";
-  } else {
-    return playerChoice;
-  }
+  );
+  return playerChoice;
 }
